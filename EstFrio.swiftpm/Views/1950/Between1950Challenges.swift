2edc9a2f3c9845dc98646569264e308f81134fed
Petehns <pedronunesufpe@gmail.com>
2025-02-21 11:43:23 -0300

    creating all viewa, only missing connecting them all

---
+++ EstFrio.swiftpm/Views/1950/Between1950Challenges.swift
@@ -0,0 +1,13 @@
+//
+//  BetweenChallenges.swift
+//  EstFrio
+//
+//  Created by Pedro Henrique Nunes da Silveira Bezerra on 21/02/25.
+//
+
+import SwiftUI
+struct Between1950Challenges: View {
+    var body: some View {
+        Text("BetweenChallenges")
+    }
+}
---
+++ EstFrio.swiftpm/Views/1950/Finished1950Challenges.swift
@@ -0,0 +1,14 @@
+//
+//  FinishedChallenges.swift
+//  EstFrio
+//
+//  Created by Pedro Henrique Nunes da Silveira Bezerra on 21/02/25.
+//
+
+import SwiftUI
+
+struct Finished1950Challenges: View {
+    var body: some View {
+        Text("Finished Challenges")
+    }
+}
